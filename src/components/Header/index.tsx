9e6fc23ffb57a29a176fd9cf0efb5416755c33bf
dvktr <victormendes04012004@gmail.com>
2025-02-27 22:49:48 -0300

    (RLP): update logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ interface HeaderProps {
 export default function Header({ headerLinks, redirectContactUs }: HeaderProps) {
   return (
     <div className="w-full h-16 bg-white flex justify-between items-center px-32 py-9">
-      <img alt="Raven logo" src="src/assets/logo-raven.png" width={188} onClick={() => {}} />
+      <img alt="Raven logo" src="public/images/logo-raven.png" width={188} onClick={() => {}} />
       <div className="flex items-center gap-8">
         <div className="header-links w-fit h-fit flex justify-center items-center gap-8">
           {headerLinks.map((link, id) => (
