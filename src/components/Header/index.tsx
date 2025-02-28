cf7250aaf5f0577d8aea1cf95c563cc93ee972b7
dvktr <victormendes04012004@gmail.com>
2025-02-27 21:41:40 -0300

    (RLP): create Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
 
 export default function Header({ headerLinks, redirectContactUs }: HeaderProps) {
   return (
-    <div className="w-full h-16 bg-primary-dark flex justify-between items-center px-32 py-9">
+    <div className="w-full h-16 bg-white flex justify-between items-center px-32 py-9">
       <img alt="Raven logo" src="src/assets/logo-raven.png" width={188} onClick={() => {}} />
       <div className="flex items-center gap-8">
         <div className="header-links w-fit h-fit flex justify-center items-center gap-8">
@@ -20,7 +20,7 @@ export default function Header({ headerLinks, redirectContactUs }: HeaderProps) 
             <a
               key={id}
               href={link.link}
-              className="text-white transition-colors duration-200 hover:text-primary-light"
+              className="text-primary-dark transition-colors duration-200 hover:text-primary-normal"
             >
               {link.name}
             </a>
