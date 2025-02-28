c19c5a12f456ce3ab7473c77e41a66a19f3219e8
dvktr <victormendes04012004@gmail.com>
2025-02-27 21:42:35 -0300

    (RLP): import Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import type { HeaderLink } from "./components/Header";
+import Header from "./components/Header";
 
+const headerLinks: HeaderLink[] = [
+  { link: "#services", name: "Serviços" },
+  { link: "#contact", name: "Contato" },
+  { link: "#about", name: "Sobre" },
+];
+
+const redirectContactUs =
+  "https://wa.me/5585989427436?text=Ol%C3%A1!%20%F0%9F%91%8B%0A%0AAcabei%20de%20visitar%20sua%20landing%20page%20e%20estou%20muito%20interessado(a)%20em%20saber%20mais%20sobre%20como%20voc%C3%AAs%20podem%20promover%20uma%20solu%C3%A7%C3%A3o%20para%20meu%20problema.";
 function App() {
-  return <div className="w-full"></div>;
+  return (
+    <div className="w-full font-sans">
+      <Header redirectContactUs={redirectContactUs} headerLinks={headerLinks} />
+    </div>
+  );
 }
 
 export default App;
