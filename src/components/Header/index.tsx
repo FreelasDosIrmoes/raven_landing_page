e4d10c1e88569d7ba301219d262edf6c40f61e09
dvktr <victormendes04012004@gmail.com>
2025-02-28 16:04:58 -0300

    (RLP): add link in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,9 +12,16 @@ interface HeaderProps {
 
 export default function Header({ headerLinks, redirectContactUs }: HeaderProps) {
   return (
-    <div className="w-full h-16 bg-white flex justify-between items-center px-32 py-9">
-      <img alt="Raven logo" src="/images/logo-raven.png" width={188} onClick={() => {}} />
-      <div className="flex items-center gap-8">
+    <div className="w-full h-fit bg-white flex justify-center md:justify-between items-center px-22 py-5">
+      <a href={"https://raventech.com.br"}>
+        <img
+          alt="Raven logo"
+          src="/images/logo-raven.png"
+          className="w-[144px] md:w-44"
+          onClick={() => {}}
+        />
+      </a>
+      <div className="md:flex hidden items-center gap-8">
         <div className="header-links w-fit h-fit flex justify-center items-center gap-8">
           {headerLinks.map((link, id) => (
             <a
