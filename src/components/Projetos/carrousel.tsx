4e7b0abd99a5ae608e889922e889433a7625627d
luanmas <luanmas27@gmail.com>
2025-03-03 10:18:49 -0300

    ajusta carrousel

--- src/components/Projetos/carrousel.tsx
+++ src/components/Projetos/carrousel.tsx
@@ -1,13 +1,35 @@
 import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css'
-import './carrousel.css'
+import Seta from "../../assets/icones/seta.svg";
+import 'swiper/swiper-bundle.css';
+import './carrousel.css';
 
 import slide_image_1 from '../../assets/image-1.png';
 import slide_image_2 from '../../assets/image-2.png';
 import slide_image_3 from '../../assets/image-3.png';
 
 function App() {
+  const projetos = [
+    {
+      nome: 'Projeto 1',
+      descricao: 'Descrição do Projeto 1. Este é um projeto incrível!',
+      tipo: 'Design',
+      imagem: slide_image_1,
+    },
+    {
+      nome: 'Projeto 2',
+      descricao: 'Descrição do Projeto 2. Este projeto é focado em desenvolvimento.',
+      tipo: 'Desenvolvimento',
+      imagem: slide_image_2,
+    },
+    {
+      nome: 'Projeto 3',
+      descricao: 'Descrição do Projeto 3. Um projeto de marketing digital.',
+      tipo: 'Marketing',
+      imagem: slide_image_3,
+    },
+  ];
+
   return (
     <div className="container">
       <Swiper
@@ -30,23 +52,33 @@ function App() {
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
       >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
-        </SwiperSlide>
+        {projetos.map((projeto, index) => (
+          <SwiperSlide key={index}>
+            <div className="slide-container">
+              <img src={projeto.imagem} alt={projeto.nome} />
+              <div className="overlay">
+                <div>
+                  <h2>{projeto.nome}</h2>
+                </div>
+                <div>
+                  <span>{projeto.tipo}</span>
+                  <p>{projeto.descricao}</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
 
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
+        {/* <div className="slider-controler">
+          <div className="swiper-button-prev">
+            <img src={Seta} />
           </div>
-          <div className="swiper-button-next slider-arrow">
+          <div className="swiper-button-next">
+            <img src={Seta} />
           </div>
-          <div className="swiper-pagination"></div>
-        </div>
+          <div className="swiper-pagination">
+          </div>
+        </div> */}
       </Swiper>
     </div>
   );
--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,9 +1,24 @@
-import Carrousel from "./carrousel"
+import Carrousel from "./carrousel";
+import Rabisco from "../../assets/acessorios/image(1).png";
 
 export const Projetos = () => {
   return (
-    <section className="p-32 flex justify-center items-center">
+    <section className="p-4 md:p-32 bg-primary-dark flex justify-center z-10 items-center flex-col">
+      <div className="relative w-full">
+        <h2 className="font-bold w-full text-3xl md:text-4xl lg:text-6xl text-white text-left z-10 relative whitespace-nowrap">
+          Nossos Projetos
+        </h2>
+        {/* <div
+          className="
+            absolute z-0
+            top-full left-0 w-[20%] h-[50%] 
+            transform translate-y-2
+          "
+        >
+          <img src={Rabisco} className="w-full h-full" />
+        </div> */}
+      </div>
       <Carrousel />
     </section>
-  )
-}
+  );
+};
