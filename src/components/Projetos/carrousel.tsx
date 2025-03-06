2883049f4fc398bf9f3037c50852bd27a8c29ea9
dvktr <victormendes04012004@gmail.com>
2025-03-06 19:47:23 -0300

    (RLP): adjust lint

--- src/components/Projetos/carrousel.tsx
+++ src/components/Projetos/carrousel.tsx
@@ -1,31 +1,30 @@
-import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Seta from "../../assets/icones/seta.svg";
-import 'swiper/swiper-bundle.css';
-import './carrousel.css';
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import "./carrousel.css";
 
-import slide_image_1 from '../../assets/image-1.png';
-import slide_image_2 from '../../assets/image-2.png';
-import slide_image_3 from '../../assets/image-3.png';
+import slide_image_1 from "../../assets/image-1.png";
+import slide_image_2 from "../../assets/image-2.png";
+import slide_image_3 from "../../assets/image-3.png";
 
 function App() {
   const projetos = [
     {
-      nome: 'Projeto 1',
-      descricao: 'Descrição do Projeto 1. Este é um projeto incrível!',
-      tipo: 'Design',
+      nome: "Projeto 1",
+      descricao: "Descrição do Projeto 1. Este é um projeto incrível!",
+      tipo: "Design",
       imagem: slide_image_1,
     },
     {
-      nome: 'Projeto 2',
-      descricao: 'Descrição do Projeto 2. Este projeto é focado em desenvolvimento.',
-      tipo: 'Desenvolvimento',
+      nome: "Projeto 2",
+      descricao: "Descrição do Projeto 2. Este projeto é focado em desenvolvimento.",
+      tipo: "Desenvolvimento",
       imagem: slide_image_2,
     },
     {
-      nome: 'Projeto 3',
-      descricao: 'Descrição do Projeto 3. Um projeto de marketing digital.',
-      tipo: 'Marketing',
+      nome: "Projeto 3",
+      descricao: "Descrição do Projeto 3. Um projeto de marketing digital.",
+      tipo: "Marketing",
       imagem: slide_image_3,
     },
   ];
@@ -33,21 +32,21 @@ function App() {
   return (
     <div className="container">
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
           depth: 100,
           modifier: 2.5,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
