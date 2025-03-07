c91c7e79e7b749c95354990744421295d5d16f10
dvktr <victormendes04012004@gmail.com>
2025-03-07 13:11:21 -0300

    (RLP): adjust imports

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ const headerLinks: HeaderLink[] = [
 ];
 
 const redirectContactUs =
-  "https://wa.me/5585989427436?text=Ol%C3%A1!%20%F0%9F%91%8B%0A%0AAcabei%20de%20visitar%20sua%20landing%20page%20e%20estou%20muito%20interessado(a)%20em%20saber%20mais%20sobre%20como%20voc%C3%AAs%20podem%20promover%20uma%20solu%C3%A7%C3%A3o%20para%20meu%20problema.";
+  "https://wa.me/5585989338909?text=Olá,%20queria%20saber%20melhor%20sobre%20os%20produtos%20e%20serviços%20da%20Raven.";
 function App() {
   return (
     <div className="w-full font-sans">
       <Header redirectContactUs={redirectContactUs} headerLinks={headerLinks} />
-      <HomeScreen />
+      <HomeScreen redirectContactUs={redirectContactUs} />
       <AdvantageScreen />
       <Projetos />
       <WorkWithUsScreen redirectContactUs={redirectContactUs} />
