64c97e15a9126de1eb2e0168031e5935edbe0d23
dvktr <victormendes04012004@gmail.com>
2025-03-07 13:11:39 -0300

    (RLP): adjust title size and description color

--- src/components/FAQScreen/index.tsx
+++ src/components/FAQScreen/index.tsx
@@ -13,41 +13,58 @@ export default function FAQScreen() {
       </div>
       <Accordion type="single" collapsible className="w-full text-primary-dark">
         <AccordionItem value="item-1">
-          <AccordionTrigger className="font-bold">
+          <AccordionTrigger className="font-bold text-md md:text-lg lg:text-xl">
             Quanto tempo leva para desenvolver um software sob medida para minha empresa?
           </AccordionTrigger>
-          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+          <AccordionContent className="text-black">
+            O tempo varia conforme a complexidade do projeto e as expectativas alinhadas em reuniões
+            iniciais. Projetos simples levar semanas; os mais complexos, de 2 a 4 meses. Na Raven,
+            definimos cronogramas realistas com você. Quer uma estimativa? Fale conosco!
+          </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger className="font-bold">
+          <AccordionTrigger className="font-bold text-md md:text-lg lg:text-xl">
             Como vocês garantem a segurança e a qualidade do código nos projetos que entregam?
           </AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other components&apos; aesthetic.
+          <AccordionContent className="text-black">
+            Na Raven, aplicamos boas práticas no desenvolvimento de software para garantir segurança
+            e qualidade do código. Nossa equipe especializada em infraestrutura assegura a
+            escalabilidade e robustez das soluções.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger className="font-bold">
+          <AccordionTrigger className="font-bold text-md md:text-lg lg:text-xl">
             Vocês oferecem suporte após a entrega do projeto?
           </AccordionTrigger>
-          <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+          <AccordionContent className="text-black">
+            Sim, na Raven, nosso compromisso com você vai além da entrega do projeto. Para todos os
+            contratos fechados, oferecemos uma garantia de 1 mês após a entrega da solução, que
+            inclui manutenção e suporte para qualquer urgência ou eventualidade que possa surgir.
+            Após esse período, oferecemos opções de suporte contínuo para atender às suas
+            necessidades a longo prazo.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
-          <AccordionTrigger className="font-bold">
+          <AccordionTrigger className="font-bold text-md md:text-lg lg:text-xl">
             Qual é o custo de uma solução de TI personalizada para minha empresa?
           </AccordionTrigger>
-          <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+          <AccordionContent className="text-black">
+            Na Raven, o custo varia conforme o escopo, complexidade e necessidades da sua empresa,
+            incluindo funcionalidades, integrações e personalização. Desenvolvemos soluções sob
+            medida com orçamentos a partir de valores acessíveis, ou oferecemos mensalidades para
+            soluções adaptadas, garantindo rapidez e economia.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-5">
-          <AccordionTrigger className="font-bold">
+          <AccordionTrigger className="font-bold text-md md:text-lg lg:text-xl">
             Como funciona o processo de contrato e acordo para os projetos da Raven?
           </AccordionTrigger>
-          <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+          <AccordionContent className="text-black">
+            Na Raven, priorizamos transparência e segurança nos contratos, feitos com suporte da
+            nossa área judicial para garantir clareza a ambas as partes. Iniciamos com reuniões de
+            alinhamento para entender suas necessidades e propor soluções, elaborando um contrato
+            detalhado que assegura um projeto bem estruturado. Quer iniciar esse processo com a
+            gente? Entre em contato pelo Email ou Whatsapp para agendar uma reunião!
           </AccordionContent>
         </AccordionItem>
       </Accordion>
