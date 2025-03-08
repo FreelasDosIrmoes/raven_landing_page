d4220f04f37da2cf1d7bf744af16422d91583d83
dvktr <victormendes04012004@gmail.com>
2025-03-08 17:08:28 -0300

    (RLP): use sonner in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import type { HeaderLink } from "./components/Header";
 import Header from "./components/Header";
 import HomeScreen from "./components/HomeScreen";
 import { Projetos } from "./components/Projetos";
+import { Toaster } from "./components/ui/sonner";
 import WorkWithUsScreen from "./components/WorkWithUsScreen";
 
 const headerLinks: HeaderLink[] = [
@@ -19,26 +20,29 @@ const redirectContactUs =
   "https://wa.me/5585989338909?text=Olá,%20queria%20saber%20melhor%20sobre%20os%20produtos%20e%20serviços%20da%20Raven.";
 function App() {
   return (
-    <div className="w-full font-sans">
-      <Header redirectContactUs={redirectContactUs} headerLinks={headerLinks} />
-      <HomeScreen redirectContactUs={redirectContactUs} />
-      <div id="benefícios">
-        <AdvantageScreen />
+    <>
+      <Toaster position="top-right" richColors />
+      <div className="w-full font-sans">
+        <Header redirectContactUs={redirectContactUs} headerLinks={headerLinks} />
+        <HomeScreen redirectContactUs={redirectContactUs} />
+        <div id="benefícios">
+          <AdvantageScreen />
+        </div>
+        <div id="projetos">
+          <Projetos />
+        </div>
+        <div id="contact">
+          <WorkWithUsScreen redirectContactUs={redirectContactUs} />
+        </div>
+        <div id="feedback">
+          <Feedback />
+        </div>
+        <div id="faq">
+          <FAQScreen />
+        </div>
+        <Footer />
       </div>
-      <div id="projetos">
-        <Projetos />
-      </div>
-      <div id="contact">
-        <WorkWithUsScreen redirectContactUs={redirectContactUs} />
-      </div>
-      <div id="feedback">
-        <Feedback />
-      </div>
-      <div id="faq">
-        <FAQScreen />
-      </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
