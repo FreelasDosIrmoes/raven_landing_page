38337e058d33b414a7df7f511be4296bc321147b
LUAN SILVA MASCARENHAS <219743@int.cagece.com.br>
2025-08-25 23:23:13 -0300

    feat: create layout with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
 import { Toaster } from "./components/ui/sonner";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Home from "../src/pages/Home";
 import Shop from "./pages/Shop";
 import ShopDetail from "./pages/ShopDetail";
+import Blog from "./pages/Blog";
+import { BlogPostPageClient } from "./pages/[slug]/BlogPost";
+import Header, { headerLinks } from "./components/Header";
+export const redirectContactUs =
+  "https://wa.me/5585989338909?text=Olá,%20queria%20saber%20melhor%20sobre%20os%20produtos%20e%20serviços%20da%20Raven.";
+
+function Layout() {
+  return (
+    <>
+      <Header headerLinks={headerLinks} redirectContactUs={redirectContactUs} />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -11,9 +25,13 @@ function App() {
       <Toaster position="top-right" richColors />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:slug" element={<ShopDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/shop/:slug" element={<ShopDetail />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogPostPageClient />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
